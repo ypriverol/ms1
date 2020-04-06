8b4363c0feca772e1c5b4109b190c645727613e8
yperez <ypriverol@gmail.com>
2020-04-06 21:12:35 +0100

    minor changes

--- analyze.py
+++ analyze.py
@@ -6,7 +6,7 @@
 # This jupyter notebook allows to analyze proteome discover output using exiting data from identified plasma proteins to re-score.
 
 # In[167]:
-
+import math
 
 import pandas as pd
 import seaborn as sns
@@ -94,6 +94,8 @@ def is_decoy(row):
         decoy = 1
     if row['Found[' + sample + ']'] == 'High' or row['Globulins'] == 1:
         decoy = 0
+    if row['Found[' + sample + ']'] == 'Not Found':
+        decoy = math.nan
     return decoy
 
 samples = []
@@ -119,9 +121,11 @@ for sample in samples:
     for index, row in dataset.iterrows():
         if row['Decoy[' + sample + ']'] == 1:
             decoy_count += 1
-        else:
+        elif row['Decoy[' + sample + ']'] == 0:
             target_count += 1
         fdr = float(decoy_count) / float(decoy_count + target_count)
+        if math.isnan(row['Decoy[' + sample + ']']):
+            fdr = math.nan
         fdr_values.append(fdr)
     dataset['FDR[' + sample + ']'] = fdr_values
 dataset.head(10)
@@ -134,53 +138,44 @@ dataset.head(10)
 # In[178]:
 
 
-def plot_targets_vs_decoy(fdr_thershold: float = 1.0, bins=20):
+def plot_targets_vs_decoy(fdr_thershold: float = 1.0, bins=20, hist=True):
     fig, axs = plt.subplots(int(len(samples) / 2), 2,
                             figsize=(15, 35))  # adjust the geometry based on your number of columns to plot
     for ax, sample in zip(axs.flatten(), samples):
-        filtered_dataset = dataset.loc[dataset['FDR[' + sample + ']'] < fdr_thershold]
-        for index, a in filtered_dataset.iterrows():
-            if sample == "S37" and fdr_thershold < 0.5:
-                print(str(a['UniprotAccession']) + " : " + str(a['AbundanceRecall[' + sample + ']']) + " : " + str(a['Decoy[' + sample + ']']))
-        targets = filtered_dataset.loc[dataset['Decoy[' + sample + ']'] == 0]
-        print("Sample -- " + sample + " Number of target proteins -- " + str(len(targets)))
-        decoys = filtered_dataset.loc[dataset['Decoy[' + sample + ']'] == 1]
-        print("Sample -- " + sample + " Number of decoy proteins -- " + str(len(decoys)))
-        ms2 = filtered_dataset.loc[dataset['Found[' + sample + ']'] == 'High']
-        ms1 = filtered_dataset.loc[dataset['Found[' + sample + ']'] == 'Peak Found']
+        filtered_dataset = dataset.loc[(dataset['FDR[' + sample + ']'] < fdr_thershold)]
+        filtered_dataset = filtered_dataset.dropna(subset=['FDR[' + sample + ']'])
+        targets = filtered_dataset.loc[filtered_dataset['Decoy[' + sample + ']'] == 0]
+        decoys = filtered_dataset.loc[filtered_dataset['Decoy[' + sample + ']'] == 1]
+
+        ms2 = filtered_dataset.loc[filtered_dataset['Found[' + sample + ']'] == 'High']
+        ms1 = filtered_dataset.loc[filtered_dataset['Found[' + sample + ']'] == 'Peak Found']
 
         targets = targets['AbundanceRecall[' + sample + ']'].to_list()
         decoys = decoys['AbundanceRecall[' + sample + ']'].to_list()
         ms2 = ms2['AbundanceRecall[' + sample + ']'].to_list()
         ms1 = ms1['AbundanceRecall[' + sample + ']'].to_list()
 
-        targets = sorted(i for i in targets if i >= 1)
-        decoys = sorted(i for i in decoys if i >= 1)
-
-        ms2 = sorted(i for i in ms2 if i >= 1)
-        ms1 = sorted(i for i in ms1 if i >= 1)
+        # targets = sorted(i for i in targets if i >= 1)
+        # decoys = sorted(i for i in decoys if i >= 1)
+        #
+        # ms2 = sorted(i for i in ms2 if i >= 1)
+        # ms1 = sorted(i for i in ms1 if i >= 1)
 
         targets = np.log2(targets)
+        targets = sorted(i for i in targets)
+
         decoys = np.log2(decoys)
         ms2 = np.log2(ms2)
         ms1 = np.log2(ms1)
+        print(targets)
+        sns.distplot(targets, bins=bins, kde=False, label='Target Proteins', hist=hist, ax=ax)
+        sns.distplot(decoys, bins=bins, kde=False, label='Decoy Proteins', hist=hist, ax=ax)
+        sns.distplot(ms2, bins=bins, kde=True, label='MS2 Proteins', hist=False, ax=ax)
+        sns.distplot(ms1, bins=bins, kde=True, label='MS1 Proteins', hist=False, ax=ax)
 
-        sns.distplot(targets, bins=bins, label='Target Proteins', ax=ax)
-        sns.distplot(decoys, bins=bins, label='Decoy Proteins', ax=ax)
-        #       sns.distplot(ms2,bins     = bins,  label='MS2 Proteins', ax = ax)
-        #       sns.distplot(ms1,bins     = bins,  label='MS1 Proteins', ax = ax)
-
-        ax.legend(labels=['Target Proteins', 'Decoy Proteins'])
+        ax.legend(labels=['Target Proteins', 'Decoy Proteins', "MS2 Proteins", "MS1 Proteins"])
 
         ax.set_title('Sample Accession -- ' + sample + " FDR Thershold: " + str(fdr_thershold))
-
-    # In[179]:
-
-
-plot_targets_vs_decoy(fdr_thershold=1.0)
-
-# In[182]:
-
 
 plot_targets_vs_decoy(fdr_thershold=0.05, bins=100)
 
